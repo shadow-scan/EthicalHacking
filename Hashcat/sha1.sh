721811fa4ca1fdb908aedd62b9ad289866cffee8
shadow-scan <crypticsignal555@gmail.com>
2025-12-29 14:18:17 +0800

    Delete Hashcat/sha1.sh

--- Hashcat/sha1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash 
-
-while read line; do 
-     echo -n "$line" | sha1sum | awk '{print $1}'
-done < /home/robot/wordlist.txt > /home/robot/sha1_hashes.txt
-
-cat /home/robot/sha1_hashes.txt
